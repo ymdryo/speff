3e2776849ecd89968cd757429dc830b8e363dd96
Ryo <ymdfield@outlook.jp>
2024-09-11 06:56:17 +0900

    Initialize experiment environment.

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Sp.Eff (spTest)
+
+main :: IO ()
+main = spTest
--- src/Sp/Eff.hs
+++ src/Sp/Eff.hs
@@ -1,94 +1,112 @@
--- |
--- Copyright: (c) 2022 Xy Ren
--- License: BSD3
--- Maintainer: xy.r@outlook.com
--- Stability: experimental
--- Portability: non-portable (GHC only)
---
--- Sp is an effect library supporting higher-order effects and scoped delimited control. It strives to be fast, sound,
--- and easy to use.
-module Sp.Eff
-  ( -- * Basic types and operations
-    Effect
-  , Eff
-  , IOE
-  , (:>)
-    -- ** Performing effects
-  , send
-    -- ** Unwrapping
-  , runIOE
-  , runEff
-    -- * Effect handling
-  , HandleTag
-  , Handler
-    -- ** Providing handlers
-  , Suffix
-  , type (++)
-    -- *** Interpret
-  , Interpret
-  , interpret
-  , interpret0
-  , interpret1
-  , interpret2
-  , interpret3
-  , interpretN
-    -- *** Interpose
-  , Interpose
-  , interpose
-  , interpose0
-  , interpose1
-  , interpose2
-  , interpose3
-  , interposeN
-    -- *** Replace
-  , Replace
-  , replace
-  , replace0
-  , replace1
-  , replace2
-  , replace3
-  , replaceN
-    -- ** Combinators to use in handlers
-  , embed
-  , withUnembed
-  , abort
-  , control
-  , Localized
-    -- * Trivial transformations
-  , KnownList
-  , KnownSubset
-    -- ** Lift
-  , Lift
-  , lift
-  , lift1
-  , lift2
-  , lift3
-  , liftN
-    -- ** Lift Under
-  , LiftNUnder
-  , liftUnder1
-  , lift1Under1
-  , lift2Under1
-  , lift3Under1
-  , liftNUnder1
-  , LiftUnderN
-  , lift1Under2
-  , lift1Under3
-  , lift1UnderN
-  , liftNUnderN
-    -- ** Subsume
-  , Subsume
-  , subsume1
-  , subsume2
-  , subsume3
-  , subsumeN
-    -- ** Miscellaneous
-  , Subset
-  , inject
-  , rearrange
-  , rearrangeN
-  ) where
+{- |
+ Copyright: (c) 2022 Xy Ren
+ License: BSD3
+ Maintainer: xy.r@outlook.com
+ Stability: experimental
+ Portability: non-portable (GHC only)
 
-import           Sp.Internal.Env    (KnownList, KnownSubset, Subset, Suffix, type (++), (:>))
-import           Sp.Internal.Handle
-import           Sp.Internal.Monad
+ Sp is an effect library supporting higher-order effects and scoped delimited control. It strives to be fast, sound,
+ and easy to use.
+-}
+module Sp.Eff (
+    -- * Basic types and operations
+    Effect,
+    Eff,
+    IOE,
+    (:>),
+
+    -- ** Performing effects
+    send,
+
+    -- ** Unwrapping
+    runIOE,
+    runEff,
+
+    -- * Effect handling
+    HandleTag,
+    Handler,
+
+    -- ** Providing handlers
+    Suffix,
+    type (++),
+
+    -- *** Interpret
+    Interpret,
+    interpret,
+    interpret0,
+    interpret1,
+    interpret2,
+    interpret3,
+    interpretN,
+
+    -- *** Interpose
+    Interpose,
+    interpose,
+    interpose0,
+    interpose1,
+    interpose2,
+    interpose3,
+    interposeN,
+
+    -- *** Replace
+    Replace,
+    replace,
+    replace0,
+    replace1,
+    replace2,
+    replace3,
+    replaceN,
+
+    -- ** Combinators to use in handlers
+    embed,
+    withUnembed,
+    abort,
+    control,
+    Localized,
+
+    -- * Trivial transformations
+    KnownList,
+    KnownSubset,
+
+    -- ** Lift
+    Lift,
+    lift,
+    lift1,
+    lift2,
+    lift3,
+    liftN,
+
+    -- ** Lift Under
+    LiftNUnder,
+    liftUnder1,
+    lift1Under1,
+    lift2Under1,
+    lift3Under1,
+    liftNUnder1,
+    LiftUnderN,
+    lift1Under2,
+    lift1Under3,
+    lift1UnderN,
+    liftNUnderN,
+
+    -- ** Subsume
+    Subsume,
+    subsume1,
+    subsume2,
+    subsume3,
+    subsumeN,
+
+    -- ** Miscellaneous
+    Subset,
+    inject,
+    rearrange,
+    rearrangeN,
+    spTest,
+) where
+
+import Sp.Internal.Env (KnownList, KnownSubset, Subset, Suffix, (:>), type (++))
+import Sp.Internal.Handle
+import Sp.Internal.Monad
+
+spTest :: IO ()
+spTest = print "test"
