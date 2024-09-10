664f8f2737589e8d926e783de954913223a4598e
Ryo <ymdfield@outlook.jp>
2024-09-11 07:27:04 +0900

    [experiment] Semantics Test.

--- src/Sp/Test.hs
+++ src/Sp/Test.hs
@@ -2,33 +2,24 @@ module Sp.Test where
 
 import Data.Kind (Type)
 import Sp.Eff
+import Sp.Error
 import Sp.Reader
 import Sp.State
 
-handleStatePure :: forall s es a. Reader s :> es => Handler (State s) es a
-handleStatePure tag = \case
-    Get ->
-        control tag \k -> do
-            s <- ask
-            k $ pure s
-    Put s ->
-        control tag \k -> do
-            local (const s) $ k $ pure ()
-    _ -> error "not implemented"
+data SomeEff :: Effect where
+    SomeAction :: SomeEff m String
 
-runStatePure :: s -> Eff (State s : es) a -> Eff es (a, s)
-runStatePure s0 m =
-    runReader s0 do
-        interpret0 handleStatePure do
-            r <- liftUnder1 m
-            s <- ask
-            pure (r, s)
+bad1 :: Either String String
+bad1 = runEff . runError @String $ do
+    interpret0 (\tag SomeAction -> embed tag $ throwError "not caught") $ do
+        send SomeAction `catchError` \(_ :: String) -> return "caught"
+
+bad2 :: String
+bad2 = runEff . runReader "unlocaled" $ do
+    interpret0 (\tag SomeAction -> embed tag ask) $ do
+        local (\_ -> "localed") $ send SomeAction
 
 spTest :: IO ()
-spTest = print @(Integer, Integer) $ runEff do
-    runStatePure 10 do
-        get @Integer >>= put . (+ 5)
-        n <- get
-        get @Integer >>= put . (+ 5)
-        get @Integer >>= put . (+ 5)
-        pure n
+spTest = do
+    print bad1
+    print bad2
