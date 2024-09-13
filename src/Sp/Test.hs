0d4f16304b1bb100d974031de71e72f2b2e158c6
Ryo <ymdfield@outlook.jp>
2024-09-13 20:13:18 +0900

    [add] experimental coroutine.

--- src/Sp/Test.hs
+++ src/Sp/Test.hs
@@ -2,21 +2,33 @@ module Sp.Test where
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Sp.Eff
-import Sp.Internal.Monad (HandleTag (HandleTag), Marker)
+import Sp.Internal.Monad (HandleTag (HandleTag), Marker, abort)
 import Sp.Reader
 import Sp.State
 
-data SomeEff :: EffectH where
-    SomeAction :: SomeEff m (SomeMarker '[])
+data Yield a b :: EffectH where
+    Yield :: a -> Yield a b m b
 
-data SomeMarker es = forall e. SomeMarker (Marker e es (SomeMarker es))
+data Status f a b r = Done r | Coroutine a (b -> f (Status f a b r))
 
-bad2 :: String
-bad2 = runPure do
-    SomeMarker mark <- interpret0 (\(HandleTag mark _) SomeAction -> pure $ SomeMarker mark) $ send SomeAction
-    _ <- interpret0 (\(HandleTag mark' evv) SomeAction -> embed (HandleTag mark evv) $ pure $ SomeMarker mark') $ send SomeAction
-    pure "a"
+runCoroutine :: forall a b es m r. Monad m => Eff m (Yield a b ': es) r -> Eff m es (Status (Eff m es) a b r)
+runCoroutine m = do
+    interpret0 (\tag (Yield a) -> control tag \k -> pure $ Coroutine a k) $ Done <$> m
 
 spTest :: IO ()
-spTest = do
-    print bad2
+spTest = runEff do
+    stat <- runCoroutine @Int @Int do
+        r <- send $ Yield @Int @Int 0
+        liftIO $ putStrLn $ "reply: " <> show r
+        _ <- send $ Yield @Int @Int 100
+        pure ()
+
+    case stat of
+        Done n -> liftIO $ print n
+        Coroutine n k -> do
+            liftIO $ putStrLn $ "yielded: " <> show n
+            stat' <- k 20
+            case stat' of
+                Done () -> liftIO $ putStrLn "Done."
+                Coroutine n' _ -> liftIO $ putStrLn $ "Continue... " <> show n'
+            pure ()
